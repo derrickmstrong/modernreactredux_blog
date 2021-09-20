9cc402ccdef399eed967eb1eba62f2c89debd930
Derrick Strong <derrickmstrong@gmail.com>
2021-09-20 10:39:40 -0500

    Added PostList UI

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -2,17 +2,30 @@ import React, { useEffect } from 'react';
 
 const PostList = props => {
   const { posts, getPosts } = props;
-
+  
   useEffect(() => {
     getPosts();
   }, [getPosts]);
+  
+  if (!posts) return null
 
-  console.log(posts);
-  const list = posts.map(({id, title}) => <p key={id}>{title}</p>)
+  const renderList = posts.map(({id, title, body}) => {
+    return (
+      <div className='item' key={id}>
+        <i className='large middle aligned icon user'></i>
+        <div className='content'>
+          <div className='description'>
+            <h2>{title}</h2>
+            <p>{body}</p>
+          </div>
+        </div>
+      </div>
+    );
+  })
 
-  return (<div>
+  return (<div className='ui relaxed divided list'>
       <h1>Post List</h1>
-      {list}
+      {renderList}
   </div>);
 };
 
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,7 +3,7 @@ import jsonPlaceholder from '../../apis/jsonPlaceholder'
 // action creator for async action ie. api call using Redux Thunk as middleware in index.js
 export const fetchPosts = () => async (dispatch, getState) => {
         const response = await jsonPlaceholder.get('/posts')
-
+        console.log(response.data)
         dispatch({
             type: 'FETCH_POSTS',
             payload: response.data
