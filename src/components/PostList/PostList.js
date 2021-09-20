cae58f28b777c5f6e60ff9a8d2c03fa2aa4831ce
Derrick Strong <derrickmstrong@gmail.com>
2021-09-20 10:26:00 -0500

    Removed class component from PostList

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,23 +1,5 @@
 import React, { useEffect } from 'react';
 
-// class PostList extends React.Component {
-
-//     componentDidMount() {
-//         // call action creater (fetchPosts) the moment the PostList is rendered to the screen
-//         this.props.fetchPosts()
-//     }
-
-//     render() {
-//         console.log(this.props.posts)
-
-//         return (
-//             <div>
-//                 Post List
-//             </div>
-//         )
-//     }
-// }
-
 const PostList = props => {
   const { posts, getPosts } = props;
 
